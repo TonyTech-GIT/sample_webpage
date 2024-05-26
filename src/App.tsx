5b52be76ca1ccdedcc41b55139f231fa8e84a793
TonyTech <toomgbebu@gmail.com>
2024-05-26 20:11:18 +0100

    first commit on sample-webpage

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.scss";
+import { Nav } from "./components";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <div className="app">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+// type Props = {};
+
+const Nav = () => {
+  return (
+    <div>
+      <h1>Hello Nav</h1>
+    </div>
+  );
+};
+
+export default Nav;
